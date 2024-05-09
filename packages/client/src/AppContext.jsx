56e2863d1601cca088f366ca04249cdd97a246e9
Mansour Koura <mansourashraf173@gmail.com>
2024-05-09 16:23:03 +0300

    enhance: using date-fns to handle dates

--- packages/client/src/AppContext.jsx
+++ packages/client/src/AppContext.jsx
@@ -1,40 +1,39 @@
-import { createContext, useState } from "react";
-import { getDateFromString } from "@utils";
+import {createContext, useState} from "react";
+import {getDateFromString} from "@utils";
 
 export const AppContext = createContext({
-  currentDate: new Date(),
-  setCurrentDate: (val) => {},
-  totalCalories: 0,
-  setTotalCalories: (val) => {},
-  currentDateStr: "",
-  isValidDate: false,
+	currentDate: new Date(),
+	setCurrentDate: (val) => {},
+	totalCalories: 0,
+	setTotalCalories: (val) => {},
+	currentDateStr: "",
+	isValidDate: false,
 });
 
 export function AppContextProvider(props) {
-  const { children } = props;
-  const [currentDate2, setCurrentDate] = useState(new Date());
-  const [totalCalories, setTotalCalories] = useState(0);
+	const {children} = props;
+	const [currentDate, setCurrentDate] = useState(getDateFromString(new Date()));
+	const [totalCalories, setTotalCalories] = useState(0);
 
-  const updateCurrentDate = (val) => {
-    setCurrentDate(getDateFromString(val));
-  };
-  console.log("Curr", currentDate);
-  const currentDateStr = !!currentDate
-    ? currentDate.toISOString().split("T")[0]
-    : "";
+	const updateCurrentDate = (val) => {
+		setCurrentDate(getDateFromString(val));
+	};
+	console.log("Curr", currentDate);
 
-  return (
-    <AppContext.Provider
-      value={{
-        currentDate,
-        setCurrentDate: updateCurrentDate,
-        totalCalories,
-        setTotalCalories,
-        currentDateStr,
-        isValidDate: !!currentDateStr,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+	const currentDateStr = currentDate ?? "";
+
+	return (
+		<AppContext.Provider
+			value={{
+				currentDate,
+				setCurrentDate: updateCurrentDate,
+				totalCalories,
+				setTotalCalories,
+				currentDateStr,
+				isValidDate: !!currentDateStr,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
 }
